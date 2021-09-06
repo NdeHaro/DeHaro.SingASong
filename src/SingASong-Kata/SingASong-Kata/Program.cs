07bb60fc2eedaf459996ee1cef99561e47276902
Eduardo Salvador <eduardo.salvador@col.vueling.com>
2021-09-06 12:26:42 +0200

    template done with tests

---
+++ src/SingASong-Kata/SingASong-Kata/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SingASong
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var tests = new SongTests();
+			tests.Test_Original_Song();
+			tests.Test_Custom_Song();
+		}
+	}
+}
